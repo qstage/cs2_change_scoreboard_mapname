5edf246833ab8cf5af4e28e4973f2868513507cc
qstage <z4haroff123@gmail.com>
2026-08-19 04:04:25 +0300

    fix: server crash with 2 players

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,12 +1,32 @@
 #include <stdio.h>
 #include "plugin.h"
 
-SH_DECL_HOOK2(INetworkMessages, Serialize, SH_NOATTRIB, 0, bool, bf_write &, const CNetMessage*);
+#include "funchook.h"
+
+std::string g_sEndingMapName;
+
+typedef bool (FASTCALL *NetworkMessageSerialize_t)(INetworkMessages*, bf_write&, const CNetMessage*);
+
+NetworkMessageSerialize_t g_pfnNetSerialize = nullptr;
+funchook_t *g_pNetSerialize = nullptr;
+constexpr int g_iNetSerializeOffset = 3;
+
+bool FASTCALL Hook_NetSerialize(INetworkMessages *pNetworkMessages, bf_write &pBuf, const CNetMessage *pData)
+{
+	NetMessageInfo_t *pMessageInfo = pData->GetNetMessage()->GetNetMessageInfo();
+
+	if (pMessageInfo->m_MessageId == svc_ClearAllStringTables)
+	{
+		CNetMessagePB<CSVCMsg_ClearAllStringTables> *pMsg = const_cast<CNetMessage*>(pData)->ToPB<CSVCMsg_ClearAllStringTables>();
+		if (pMsg) pMsg->set_mapname(pMsg->mapname() + g_sEndingMapName.c_str());
+	}
+
+	return g_pfnNetSerialize(pNetworkMessages, pBuf, pData);
+}
+
 
 MMSPlugin g_ThisPlugin;
 PLUGIN_EXPOSE(MMSPlugin, g_ThisPlugin);
-
-std::string g_sEndingMapName;
 
 bool MMSPlugin::Load(PluginId id, ISmmAPI *ismm, char *error, size_t maxlen, bool late)
 {
@@ -17,7 +37,12 @@ bool MMSPlugin::Load(PluginId id, ISmmAPI *ismm, char *error, size_t maxlen, boo
 	GET_V_IFACE_ANY(GetFileSystemFactory, g_pFullFileSystem, IFileSystem, FILESYSTEM_INTERFACE_VERSION);
 	GET_V_IFACE_CURRENT(GetEngineFactory, g_pNetworkMessages, INetworkMessages, NETWORKMESSAGES_INTERFACE_VERSION);
 
-	SH_ADD_HOOK(INetworkMessages, Serialize, g_pNetworkMessages, SH_MEMBER(this, &MMSPlugin::Serialize), false);
+	void** pNetworkMessagesVtable = *(void***)g_pNetworkMessages;
+	g_pfnNetSerialize = (NetworkMessageSerialize_t)pNetworkMessagesVtable[g_iNetSerializeOffset];
+
+	g_pNetSerialize = funchook_create();
+	funchook_prepare(g_pNetSerialize, (void **)&g_pfnNetSerialize, (void *)Hook_NetSerialize);
+	funchook_install(g_pNetSerialize, 0);
 
 	g_SMAPI->AddListener( this, this );
 
@@ -35,27 +60,13 @@ bool MMSPlugin::Load(PluginId id, ISmmAPI *ismm, char *error, size_t maxlen, boo
 	return true;
 }
 
-bool MMSPlugin::Serialize(bf_write &pBuf, const CNetMessage *pData)
-{
-	if (pData == nullptr)
-		RETURN_META_VALUE(MRES_IGNORED, false);
-
-	NetMessageInfo_t *pMessageInfo = pData->GetNetMessage()->GetNetMessageInfo();
-
-	if (pMessageInfo && pMessageInfo->m_MessageId == svc_ClearAllStringTables)
-	{
-		CNetMessagePB<CSVCMsg_ClearAllStringTables> *pMsg = const_cast<CNetMessage*>(pData)->ToPB<CSVCMsg_ClearAllStringTables>();
-
-		if (pMsg)
-			pMsg->set_mapname(pMsg->mapname() + g_sEndingMapName.c_str());
-	}
-
-	RETURN_META_VALUE(MRES_IGNORED, true);
-}
-
 bool MMSPlugin::Unload(char *error, size_t maxlen)
 {
-	SH_REMOVE_HOOK(INetworkMessages, Serialize, g_pNetworkMessages, SH_MEMBER(this, &MMSPlugin::Serialize), false);
+	if (g_pNetSerialize)
+	{
+		funchook_uninstall(g_pNetSerialize, 0);
+		funchook_destroy(g_pNetSerialize);
+	}
 
 	return true;
 }
